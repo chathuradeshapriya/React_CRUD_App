faf62c0e972913a6cb62b58bda71ace98892004f
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-09-08 03:55:27 +0530

    Add CSS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
 
 function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">        </div>
+        <div className="previous-operand"></div>
         <div className="current-operand"></div>
       </div>
 
-      <button
-        className="span-two">
-        AC</button>
+      <button className="span-two">AC</button>
       <button> DEL </button>
-
-
-      <button> / </button>
+     <button> / </button>
       <button>1</button>
       <button>2</button>
       <button>3</button>
